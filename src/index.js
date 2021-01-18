2cdffbf1e3ef4f4518bc989eae83c0b5878614a5
s1ga <s.v.harlanov@gmail.com>
2021-01-18 13:31:02 +0300

    feat: implement reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const reverse = n.toString().split('').reverse()
+    if (reverse.includes('-')) {
+        reverse.splice(reverse.length - 1, 1)
+    }
+
+    return +reverse.join('')
 }
